3e3854ff8cc6cdc9c4480f3d544be9d0722bc099
codingjogo <codingjogo@gmail.com>
2023-12-17 19:08:19 +0800

    Add project files.

---
+++ EmployeeManagementSystem/LoginForm.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace EmployeeManagementSystem
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.leftPanel = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.usernameTxtbox = new System.Windows.Forms.TextBox();
+            this.passwordTxtbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.showpassCheckbox = new System.Windows.Forms.CheckBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.signupButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.leftPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // leftPanel
+            // 
+            this.leftPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(15)))), ((int)(((byte)(245)))));
+            this.leftPanel.Controls.Add(this.label5);
+            this.leftPanel.Controls.Add(this.signupButton);
+            this.leftPanel.Controls.Add(this.label4);
+            this.leftPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.leftPanel.Location = new System.Drawing.Point(0, 0);
+            this.leftPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.leftPanel.Name = "leftPanel";
+            this.leftPanel.Size = new System.Drawing.Size(354, 411);
+            this.leftPanel.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Controls.Add(this.loginButton);
+            this.panel1.Controls.Add(this.showpassCheckbox);
+            this.panel1.Controls.Add(this.passwordTxtbox);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.usernameTxtbox);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(354, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(20);
+            this.panel1.Size = new System.Drawing.Size(280, 411);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(0, 0, 0, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 66);
+            this.label2.Margin = new System.Windows.Forms.Padding(0, 0, 0, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username:";
+            // 
+            // usernameTxtbox
+            // 
+            this.usernameTxtbox.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameTxtbox.Location = new System.Drawing.Point(23, 91);
+            this.usernameTxtbox.Margin = new System.Windows.Forms.Padding(0, 0, 0, 16);
+            this.usernameTxtbox.Multiline = true;
+            this.usernameTxtbox.Name = "usernameTxtbox";
+            this.usernameTxtbox.Size = new System.Drawing.Size(230, 32);
+            this.usernameTxtbox.TabIndex = 2;
+            // 
+            // passwordTxtbox
+            // 
+            this.passwordTxtbox.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTxtbox.Location = new System.Drawing.Point(23, 164);
+            this.passwordTxtbox.Margin = new System.Windows.Forms.Padding(0, 0, 0, 4);
+            this.passwordTxtbox.Multiline = true;
+            this.passwordTxtbox.Name = "passwordTxtbox";
+            this.passwordTxtbox.Size = new System.Drawing.Size(230, 32);
+            this.passwordTxtbox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 139);
+            this.label3.Margin = new System.Windows.Forms.Padding(0, 0, 0, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password:";
+            // 
+            // showpassCheckbox
+            // 
+            this.showpassCheckbox.AutoSize = true;
+            this.showpassCheckbox.Location = new System.Drawing.Point(136, 203);
+            this.showpassCheckbox.Margin = new System.Windows.Forms.Padding(0, 0, 0, 32);
+            this.showpassCheckbox.Name = "showpassCheckbox";
+            this.showpassCheckbox.Size = new System.Drawing.Size(117, 20);
+            this.showpassCheckbox.TabIndex = 5;
+            this.showpassCheckbox.Text = "Show password";
+            this.showpassCheckbox.UseVisualStyleBackColor = true;
+            this.showpassCheckbox.CheckedChanged += new System.EventHandler(this.showpassCheckbox_CheckedChanged);
+            // 
+            // loginButton
+            // 
+            this.loginButton.AutoSize = true;
+            this.loginButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(175)))), ((int)(((byte)(15)))), ((int)(((byte)(255)))));
+            this.loginButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loginButton.FlatAppearance.BorderSize = 0;
+            this.loginButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loginButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.ForeColor = System.Drawing.Color.White;
+            this.loginButton.Location = new System.Drawing.Point(23, 255);
+            this.loginButton.Margin = new System.Windows.Forms.Padding(0);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Padding = new System.Windows.Forms.Padding(4);
+            this.loginButton.Size = new System.Drawing.Size(230, 40);
+            this.loginButton.TabIndex = 6;
+            this.loginButton.Text = "LOGIN";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(18, 139);
+            this.label4.Margin = new System.Windows.Forms.Padding(0, 0, 0, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(324, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Employee Management System";
+            // 
+            // signupButton
+            // 
+            this.signupButton.AutoSize = true;
+            this.signupButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(175)))), ((int)(((byte)(15)))), ((int)(((byte)(255)))));
+            this.signupButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.signupButton.FlatAppearance.BorderSize = 0;
+            this.signupButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.signupButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signupButton.ForeColor = System.Drawing.Color.White;
+            this.signupButton.Location = new System.Drawing.Point(58, 349);
+            this.signupButton.Margin = new System.Windows.Forms.Padding(0, 4, 0, 0);
+            this.signupButton.Name = "signupButton";
+            this.signupButton.Padding = new System.Windows.Forms.Padding(4);
+            this.signupButton.Size = new System.Drawing.Size(230, 40);
+            this.signupButton.TabIndex = 7;
+            this.signupButton.Text = "SIGN UP";
+            this.signupButton.UseVisualStyleBackColor = false;
+            this.signupButton.Click += new System.EventHandler(this.signupButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(113, 319);
+            this.label5.Margin = new System.Windows.Forms.Padding(0, 0, 0, 8);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 18);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Register account";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 411);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.leftPanel);
+            this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoginForm";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.leftPanel.ResumeLayout(false);
+            this.leftPanel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel leftPanel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox passwordTxtbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox usernameTxtbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.CheckBox showpassCheckbox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button signupButton;
+    }
+}
---
+++ EmployeeManagementSystem/LoginForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeManagementSystem
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void signupButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void showpassCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
