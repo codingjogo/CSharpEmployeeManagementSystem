ba67f316787379fd3c49714ccd521c68b56026bc
codingjogo <codingjogo@gmail.com>
2023-12-21 17:36:31 +0800

    some needs TODO

--- EmployeeManagementSystem/AddEmployeeUserControl.Designer.cs
+++ EmployeeManagementSystem/AddEmployeeUserControl.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.addEmployee_clear_button = new System.Windows.Forms.Button();
             this.addEmployee_delete_button = new System.Windows.Forms.Button();
             this.addEmployee_update_button = new System.Windows.Forms.Button();
@@ -40,15 +41,17 @@
             this.label3 = new System.Windows.Forms.Label();
             this.addEmployee_id_txtbox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.employeeData_dgv = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.addEmployee_status_cmb = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.addEmployee_position_cmb = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.addEmployee_uploadPic_button = new System.Windows.Forms.Button();
             this.addEmployee_picture_picBox = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.addEmployee_position_cmb = new System.Windows.Forms.ComboBox();
-            this.label6 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeData_dgv)).BeginInit();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.addEmployee_picture_picBox)).BeginInit();
             this.panel1.SuspendLayout();
@@ -71,6 +74,7 @@
             this.addEmployee_clear_button.TabIndex = 12;
             this.addEmployee_clear_button.Text = "CLEAR";
             this.addEmployee_clear_button.UseVisualStyleBackColor = false;
+            this.addEmployee_clear_button.Click += new System.EventHandler(this.addEmployee_clear_button_Click);
             // 
             // addEmployee_delete_button
             // 
@@ -89,6 +93,7 @@
             this.addEmployee_delete_button.TabIndex = 11;
             this.addEmployee_delete_button.Text = "DELETE";
             this.addEmployee_delete_button.UseVisualStyleBackColor = false;
+            this.addEmployee_delete_button.Click += new System.EventHandler(this.addEmployee_delete_button_Click);
             // 
             // addEmployee_update_button
             // 
@@ -107,6 +112,7 @@
             this.addEmployee_update_button.TabIndex = 10;
             this.addEmployee_update_button.Text = "UPDATE";
             this.addEmployee_update_button.UseVisualStyleBackColor = false;
+            this.addEmployee_update_button.Click += new System.EventHandler(this.addEmployee_update_button_Click);
             // 
             // addEmployee_add_button
             // 
@@ -125,6 +131,7 @@
             this.addEmployee_add_button.TabIndex = 9;
             this.addEmployee_add_button.Text = "ADD";
             this.addEmployee_add_button.UseVisualStyleBackColor = false;
+            this.addEmployee_add_button.Click += new System.EventHandler(this.addEmployee_add_button_Click);
             // 
             // addEmployee_phoneNo_txtbox
             // 
@@ -212,13 +219,20 @@
             this.label2.TabIndex = 0;
             this.label2.Text = "Employee ID:";
             // 
-            // dataGridView1
+            // employeeData_dgv
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(15, 49);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(737, 376);
-            this.dataGridView1.TabIndex = 1;
+            this.employeeData_dgv.AllowUserToAddRows = false;
+            this.employeeData_dgv.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.LightGray;
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
+            this.employeeData_dgv.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.employeeData_dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.employeeData_dgv.Location = new System.Drawing.Point(15, 49);
+            this.employeeData_dgv.Name = "employeeData_dgv";
+            this.employeeData_dgv.ReadOnly = true;
+            this.employeeData_dgv.Size = new System.Drawing.Size(737, 376);
+            this.employeeData_dgv.TabIndex = 1;
+            this.employeeData_dgv.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.employeeData_dgv_CellClick);
             // 
             // label1
             // 
@@ -233,6 +247,8 @@
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.addEmployee_status_cmb);
+            this.panel2.Controls.Add(this.label7);
             this.panel2.Controls.Add(this.addEmployee_position_cmb);
             this.panel2.Controls.Add(this.label6);
             this.panel2.Controls.Add(this.addEmployee_uploadPic_button);
@@ -255,42 +271,28 @@
             this.panel2.Size = new System.Drawing.Size(763, 208);
             this.panel2.TabIndex = 3;
             // 
-            // addEmployee_uploadPic_button
+            // addEmployee_status_cmb
             // 
-            this.addEmployee_uploadPic_button.AutoSize = true;
-            this.addEmployee_uploadPic_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(175)))), ((int)(((byte)(15)))), ((int)(((byte)(255)))));
-            this.addEmployee_uploadPic_button.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.addEmployee_uploadPic_button.FlatAppearance.BorderSize = 0;
-            this.addEmployee_uploadPic_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.addEmployee_uploadPic_button.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.addEmployee_uploadPic_button.ForeColor = System.Drawing.Color.White;
-            this.addEmployee_uploadPic_button.Image = global::EmployeeManagementSystem.Properties.Resources.icons8_logout_rounded_up_filled_25px1;
-            this.addEmployee_uploadPic_button.Location = new System.Drawing.Point(672, 97);
-            this.addEmployee_uploadPic_button.Margin = new System.Windows.Forms.Padding(0);
-            this.addEmployee_uploadPic_button.Name = "addEmployee_uploadPic_button";
-            this.addEmployee_uploadPic_button.Padding = new System.Windows.Forms.Padding(4);
-            this.addEmployee_uploadPic_button.Size = new System.Drawing.Size(80, 39);
-            this.addEmployee_uploadPic_button.TabIndex = 13;
-            this.addEmployee_uploadPic_button.UseVisualStyleBackColor = false;
+            this.addEmployee_status_cmb.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addEmployee_status_cmb.FormattingEnabled = true;
+            this.addEmployee_status_cmb.Items.AddRange(new object[] {
+            "Active",
+            "Inactive"});
+            this.addEmployee_status_cmb.Location = new System.Drawing.Point(450, 97);
+            this.addEmployee_status_cmb.Name = "addEmployee_status_cmb";
+            this.addEmployee_status_cmb.Size = new System.Drawing.Size(200, 32);
+            this.addEmployee_status_cmb.TabIndex = 17;
             // 
-            // addEmployee_picture_picBox
+            // label7
             // 
-            this.addEmployee_picture_picBox.Location = new System.Drawing.Point(672, 11);
-            this.addEmployee_picture_picBox.Name = "addEmployee_picture_picBox";
-            this.addEmployee_picture_picBox.Size = new System.Drawing.Size(80, 80);
-            this.addEmployee_picture_picBox.TabIndex = 8;
-            this.addEmployee_picture_picBox.TabStop = false;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.dataGridView1);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(22, 23);
-            this.panel1.Margin = new System.Windows.Forms.Padding(0, 0, 0, 16);
-            this.panel1.Name = "panel1";
-            this.panel1.Padding = new System.Windows.Forms.Padding(8);
-            this.panel1.Size = new System.Drawing.Size(763, 436);
-            this.panel1.TabIndex = 2;
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(353, 103);
+            this.label7.Margin = new System.Windows.Forms.Padding(3, 0, 8, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 19);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Status:";
             // 
             // addEmployee_position_cmb
             // 
@@ -316,6 +318,44 @@
             this.label6.TabIndex = 14;
             this.label6.Text = "Position:";
             // 
+            // addEmployee_uploadPic_button
+            // 
+            this.addEmployee_uploadPic_button.AutoSize = true;
+            this.addEmployee_uploadPic_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(175)))), ((int)(((byte)(15)))), ((int)(((byte)(255)))));
+            this.addEmployee_uploadPic_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.addEmployee_uploadPic_button.FlatAppearance.BorderSize = 0;
+            this.addEmployee_uploadPic_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addEmployee_uploadPic_button.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addEmployee_uploadPic_button.ForeColor = System.Drawing.Color.White;
+            this.addEmployee_uploadPic_button.Image = global::EmployeeManagementSystem.Properties.Resources.icons8_logout_rounded_up_filled_25px1;
+            this.addEmployee_uploadPic_button.Location = new System.Drawing.Point(672, 97);
+            this.addEmployee_uploadPic_button.Margin = new System.Windows.Forms.Padding(0);
+            this.addEmployee_uploadPic_button.Name = "addEmployee_uploadPic_button";
+            this.addEmployee_uploadPic_button.Padding = new System.Windows.Forms.Padding(4);
+            this.addEmployee_uploadPic_button.Size = new System.Drawing.Size(80, 39);
+            this.addEmployee_uploadPic_button.TabIndex = 13;
+            this.addEmployee_uploadPic_button.UseVisualStyleBackColor = false;
+            this.addEmployee_uploadPic_button.Click += new System.EventHandler(this.addEmployee_uploadPic_button_Click);
+            // 
+            // addEmployee_picture_picBox
+            // 
+            this.addEmployee_picture_picBox.Location = new System.Drawing.Point(672, 11);
+            this.addEmployee_picture_picBox.Name = "addEmployee_picture_picBox";
+            this.addEmployee_picture_picBox.Size = new System.Drawing.Size(80, 80);
+            this.addEmployee_picture_picBox.TabIndex = 8;
+            this.addEmployee_picture_picBox.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.employeeData_dgv);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(22, 23);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0, 0, 0, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(8);
+            this.panel1.Size = new System.Drawing.Size(763, 436);
+            this.panel1.TabIndex = 2;
+            // 
             // AddEmployeeUserControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -325,7 +365,7 @@
             this.Name = "AddEmployeeUserControl";
             this.Padding = new System.Windows.Forms.Padding(20);
             this.Size = new System.Drawing.Size(806, 709);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeData_dgv)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.addEmployee_picture_picBox)).EndInit();
@@ -351,11 +391,13 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox addEmployee_id_txtbox;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView employeeData_dgv;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.ComboBox addEmployee_position_cmb;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox addEmployee_status_cmb;
+        private System.Windows.Forms.Label label7;
     }
 }
